d4259af9ab6e36004df700ffaab117e7d4aa9d98
Luis Reyes <yuiftw05@gmail.com>
2026-04-29 06:54:43 -0600

    store fonts, colors and featured section styles started

--- my-medusa-store-storefront/src/modules/home/components/hero/index.tsx
+++ my-medusa-store-storefront/src/modules/home/components/hero/index.tsx
@@ -3,18 +3,18 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full relative bg-gradient-to-b from-brand-abyss-purple to-brand-void-black flex items-center justify-center">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-3xl leading-10 text-brand-sacred-violet font-display font-normal"
           >
-            Ecommerce Starter Template
+             Y2k Fit Store
           </Heading>
           <Heading
             level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-3xl leading-10 text-brand-silver-ash font-heading font-normal"
           >
             Powered by Medusa and Next.js
           </Heading>
@@ -23,10 +23,6 @@ const Hero = () => {
           href="https://github.com/medusajs/nextjs-starter-medusa"
           target="_blank"
         >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
         </a>
       </div>
     </div>
