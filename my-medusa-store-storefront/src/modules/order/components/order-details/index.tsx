0db447e43c0af91c38c49b914b3e454dc28c2274
Luis Reyes <yuiftw05@gmail.com>
2026-05-13 02:09:52 -0600

    feat: adding new navbar styles and adding payment methods icons

--- my-medusa-store-storefront/src/modules/order/components/order-details/index.tsx
+++ my-medusa-store-storefront/src/modules/order/components/order-details/index.tsx
@@ -18,7 +18,7 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
       <Text>
         We have sent the order confirmation details to{" "}
         <span
-          className="text-ui-fg-medium-plus font-semibold"
+          className="text-brand-ghost-white font-semibold"
           data-testid="order-email"
         >
           {order.email}
@@ -31,7 +31,7 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
           {new Date(order.created_at).toDateString()}
         </span>
       </Text>
-      <Text className="mt-2 text-ui-fg-interactive">
+      <Text className="mt-2 text-brand-sacred-violet">
         Order number: <span data-testid="order-id">{order.display_id}</span>
       </Text>
 
@@ -40,14 +40,14 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
           <>
             <Text>
               Order status:{" "}
-              <span className="text-ui-fg-subtle " data-testid="order-status">
+              <span className="text-brand-silver-ash " data-testid="order-status">
                 {formatStatus(order.fulfillment_status)}
               </span>
             </Text>
             <Text>
               Payment status:{" "}
               <span
-                className="text-ui-fg-subtle "
+                className="text-brand-silver-ash "
                 sata-testid="order-payment-status"
               >
                 {formatStatus(order.payment_status)}
--- my-medusa-store-storefront/src/modules/order/components/payment-details/index.tsx
+++ my-medusa-store-storefront/src/modules/order/components/payment-details/index.tsx
@@ -14,29 +14,29 @@ const PaymentDetails = ({ order }: PaymentDetailsProps) => {
 
   return (
     <div>
-      <Heading level="h2" className="flex flex-row text-3xl-regular my-6">
+      <Heading level="h2" className="flex flex-row text-3xl-regular font-heading text-brand-ghost-white my-6">
         Payment
       </Heading>
       <div>
         {payment && (
           <div className="flex items-start gap-x-1 w-full">
             <div className="flex flex-col w-1/3">
-              <Text className="txt-medium-plus text-ui-fg-base mb-1">
+              <Text className="txt-medium-plus text-brand-ghost-white mb-1">
                 Payment method
               </Text>
               <Text
-                className="txt-medium text-ui-fg-subtle"
+                className="txt-medium text-brand-silver-ash"
                 data-testid="payment-method"
               >
                 {paymentInfoMap[payment.provider_id].title}
               </Text>
             </div>
             <div className="flex flex-col w-2/3">
-              <Text className="txt-medium-plus text-ui-fg-base mb-1">
+              <Text className="txt-medium-plus text-brand-ghost-white mb-1">
                 Payment details
               </Text>
-              <div className="flex gap-2 txt-medium text-ui-fg-subtle items-center">
-                <Container className="flex items-center h-7 w-fit p-2 bg-ui-button-neutral-hover">
+              <div className="flex gap-2 txt-medium text-brand-silver-ash items-center">
+                <Container className="flex items-center h-7 w-fit p-2 bg-brand-void-black border border-brand-amethyst rounded-base">
                   {paymentInfoMap[payment.provider_id].icon}
                 </Container>
                 <Text data-testid="payment-amount">
